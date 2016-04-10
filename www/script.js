51c5d36e2548e8403e46e578a3ec0eca73d144aa
Thomas-Redding <thomas@carlwireless-185-249.dyn.carleton.edu>
2016-04-10 00:27:25 -0500

    uses cookies to store username and password

--- www/script.js
+++ www/script.js
@@ -14,8 +14,11 @@ function login() {
 		// log in
 		username = prompt("username:");
 		password = prompt("password:");
-		if(isValidString(username) || isValidString(password))
+		if(isValidString(username) || isValidString(password)) {
+			setCookie("username", username, 365);
+			setCookie("password", password, 365);
 			document.getElementById("login-link").innerHTML = "Sign Out of " + username + "?";
+		}
 		else
 			alert("Invalid Username Or Password");
 	}
@@ -23,6 +26,8 @@ function login() {
 		// sign out
 		username = "";
 		password = "";
+		setCookie("username", username, 365);
+		setCookie("password", password, 365);
 		document.getElementById("login-link").innerHTML = "Login";
 	}
 }
@@ -42,6 +47,8 @@ function ajax(txt) {
 					password = arr[1];
 					if(username != "" && password != "") {
 						document.getElementById("login-link").innerHTML = "Sign Out of " + username + "?";
+						setCookie("username", username, 365);
+						setCookie("password", password, 365);
 					}
 				}
 			}
@@ -50,4 +57,22 @@ function ajax(txt) {
 	};
 	ajaxObj.open("GET",txt,true);
 	ajaxObj.send();
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
 }
